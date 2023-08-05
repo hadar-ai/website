12fff57e71b1ebd92d2f62d95bf23e2907e65fa1
Ramesh Nair <ram@hiddentao.com>
2023-08-05 17:09:58 +0100

    page: privacy policy

--- src/app/privacy/page.tsx
+++ src/app/privacy/page.tsx
@@ -1,10 +1,13 @@
+import { Markdown } from '@/components/Markdown'
+// @ts-ignore
+import privacyContent from './privacy.md' 
+import { Content } from '@/components/Content'
+
+
 export default function Privacy() {
   return (
-    <main className='first_block'>
-      <div className="container mt-32">
-        Privacy policy coming soon!
-      </div>
-    </main>
+    <Content className="pb-20">
+      <Markdown markdown={privacyContent} />
+    </Content>
   )
 }
-
